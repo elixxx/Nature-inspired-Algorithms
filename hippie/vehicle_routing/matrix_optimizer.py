e249ab70cb0df8904215bf43c95440514d14b1fb
StatefulMind <17194694+StatefulMind@users.noreply.github.com>
2018-02-01 10:58:11 +0100

    Optimized matrix optimizer and introduced convergence criterion

--- hippie/vehicle_routing/matrix_optimizer.py
+++ hippie/vehicle_routing/matrix_optimizer.py
@@ -2,7 +2,12 @@ import parser
 import numpy as np
 
 
-def optimize_distances(distances, depth = 1):
+def optimize_distances(distances, depth = 0):
+    """Optimizes a distance matrix for depth steps or until convergence"""
+    if depth == 0:
+        #Dirty method to iterate until convergence
+        depth = np.infty
+
     dim_x, dim_y = distances.shape
     opt_dist = distances.copy()
 
@@ -11,7 +16,12 @@ def optimize_distances(distances, depth = 1):
     opt_via.fill([])
     opt_via = np.frompyfunc(list, 1, 1)(opt_via)
 
-    for iteration in range(depth):
+    iteration = 0
+    while True:
+        if iteration >= depth:
+            break
+
+        opt_dist_old = opt_dist.copy()
         for from_y in range(dim_y):
             for to_x in range(from_y+1, dim_x):
                 vias = [opt_dist[from_y, via] + opt_dist[via, to_x] for via in range(dim_y)]
@@ -24,7 +34,18 @@ def optimize_distances(distances, depth = 1):
                 opt_dist[to_x, from_y] = opt_dist[from_y, to_x]
                 opt_via[to_x, from_y] = list(reversed(opt_via[from_y, to_x]))
 
+        if (opt_dist_old == opt_dist).all():
+            #If no changes in otp_dist happened in one iteration, stop optimization
+            print("Convergence!")
+            break
+        iteration += 1
+
     return opt_dist, opt_via
+
+def print_optimization(distances, opt_distances):
+    ratios = 1-np.divide(opt_distances+0.0001, distances+0.0001) # Dirty method to avoid division by 0
+    avg_ratio = np.average(ratios)
+    print("The distances were optimized by an average of ~{}%".format(avg_ratio*100))
 
 
 matrix_via1 = np.mat([[0, 10, 2], [10, 0, 3], [2, 3, 0]])
@@ -36,10 +57,12 @@ file = "../../problems/vehicle_routing/distance.txt"
 distances = parser.parse_to_matrix(file)
 matrix = distances
 
-opt_dist, opt_via = optimize_distances(matrix, depth = 4)
+opt_dist, opt_via = optimize_distances(matrix, depth = 0)
 print("Simple matrix")
 print(matrix)
 
 print("Optimized matrix")
 print(opt_dist)
 print(opt_via)
+
+print_optimization(matrix, opt_dist)
